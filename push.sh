bf1e5e1c5a11af7c55ded7bc4344a10e5b601c29
Karl <karldev@fastmail.com>
2025-04-05 17:57:39 +0100

    Journal posts

--- push.sh
+++ push.sh
@@ -5,7 +5,7 @@
 git add --all
 git commit -m "Journal posts"
 git push origin main
-git push local main
+git push origin2 main
 
 git -C ../template add --all
 git -C ../template commit -m "Journal posts"
